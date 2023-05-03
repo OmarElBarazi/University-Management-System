032bd6115c95850e377ce97a6688f6667202c077
Omar7br <omar7br@gmail.com>
2023-05-03 23:26:15 +0300

    User controllers

---
+++ src/models/userModel.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+const {
+  generateStudentId,
+  hashPassword,
+} = require("../pre-functions/userPreFunctions");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "staff", "student"],
+    required: true,
+  },
+  studentId: {
+    type: Number,
+    unique: true,
+  },
+  advisor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  startDate: {
+    type: String,
+    required: true,
+    match: /^(0?[1-9]|[12][0-9]|3[01])\/(0?[1-9]|1[0-2])\/\d{4}$/,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+  semester: {
+    type: String,
+    enum: ["fall", "spring"],
+    required: true,
+  },
+});
+
+userSchema.pre("save", await hashPassword);
+userSchema.pre("save", await generateStudentId);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
